61e6027b2f36782f32c8af7ac35f54442579323c
Abheek246 <abheeksrivastava36@gmail.com>
2025-07-07 21:40:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_instance" "my-instance" {
    ami = var.ami-id
    key_name = var.key-name
    instance_type = var.instance-type
-   vpc_security_group_ids = [aws_security_group_security_group_1_id]
+   vpc_security_group_ids = [aws_security_group.security_group_1.id]
    tags = {
     Name = "my_EC2_instance"
    }
